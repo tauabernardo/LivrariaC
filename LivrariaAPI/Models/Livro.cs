e8b9e86ab1f4c35ecd0218e83d2f9dc61c3e30e9
Tauã Bernardo <tbptffc@gmail.com>
2025-02-10 16:31:43 -0300

    Criação da classe Livro

---
+++ LivrariaAPI/Models/Livro.cs
@@ -0,0 +1,10 @@
+namespace LivrariaAPI.Models
+{
+    public abstract class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+        public int AnoPublicacao { get; set; }
+    }
+}
